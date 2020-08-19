e5dc87ae09a1c13d321da94c049b3460a649e4a5
Tam O'Donnell <thomashodonnell@gmail.com>
2020-08-19 13:21:28 +0100

    add develop

--- db.tf
+++ db.tf
@@ -21,20 +21,21 @@ resource "aws_dynamodb_table" "basic-dynamodb-table" {
     type = "N"
   }
 
-#   ttl {
-#     attribute_name = "TimeToExist"
-#     enabled        = false
-#   }
+  #   ttl {
+  #     attribute_name = "TimeToExist"
+  #     enabled        = false
+  #   }
 
-#   global_secondary_index {
-#     name               = "GameTitleIndex"
-#     hash_key           = "GameTitle"
-#     range_key          = "TopScore"
-#     write_capacity     = 10
-#     read_capacity      = 10
-#     projection_type    = "INCLUDE"
-#     non_key_attributes = ["UserId"]
-#   }
+  #   global_secondary_index {
+  #     name               = "GameTitleIndex"
+  #     hash_key           = "GameTitle"
+  #     range_key          = "TopScore"
+  #     write_capacity     = 10
+  #     read_capacity      = 10
+  #     projection_type    = "INCLUDE"
+  #     non_key_attributes = ["UserId"]
+  #   }
+
 
   tags = {
     Name        = "dynamodb-table-1"
